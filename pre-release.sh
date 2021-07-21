3d4746f623ec0032190c5c9eccd82a49b495bfc9
Douglas Naphas <douglasnaphas@gmail.com>
2021-07-20 20:38:56 -0400

    Properly refer to tags in pre-release.sh

--- pre-release.sh
+++ pre-release.sh
@@ -1,11 +1,11 @@
-PACKAGE_JSON_VERSION=refs/heads/v$(cat package.json | jq '.version' | tr -d '"')
+PACKAGE_JSON_VERSION=refs/tags/v$(cat package.json | jq '.version' | tr -d '"')
 if [[ "${PACKAGE_JSON_VERSION}" != "${GITHUB_REF}" ]]
 then
   echo -n "error: PACKAGE_JSON_VERSION ${PACKAGE_JSON_VERSION} mismatches "
   echo    "GITHUB_REF ${GITHUB_REF}"
   exit 1
 fi
-CLI_VERSION=refs/heads/v$(npx . --version)
+CLI_VERSION=refs/tags/v$(npx . --version)
 if [[ "${CLI_VERSION}" != "${GITHUB_REF}" ]]
 then
   echo -n "error: CLI_VERSION ${CLI_VERSION} mismatches "
