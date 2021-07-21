cfdbf04c8a629478a3f00309e7c641bfea5dae71
Douglas Naphas <douglasnaphas@gmail.com>
2021-07-20 20:53:06 -0400

    Check package-lock.json version

--- pre-release.sh
+++ pre-release.sh
@@ -5,6 +5,13 @@ then
   echo    "GITHUB_REF ${GITHUB_REF}"
   exit 1
 fi
+PACKAGE_LOCK_JSON_VERSION=refs/tags/v$(cat package-lock.json | jq '.version' | tr -d '"')
+if [[ "${PACKAGE_LOCK_JSON_VERSION}" != "${GITHUB_REF}" ]]
+then
+  echo -n "error: PACKAGE_LOCK_JSON_VERSION ${PACKAGE_LOCK_JSON_VERSION} mismatches "
+  echo    "GITHUB_REF ${GITHUB_REF}"
+  exit 1
+fi
 CLI_VERSION=refs/tags/v$(npx . --version)
 if [[ "${CLI_VERSION}" != "${GITHUB_REF}" ]]
 then
